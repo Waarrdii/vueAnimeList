250ac2f34026698300c3aa2c8e7a632edf63c3d5
waarrdii <waarrdii@gmail.com>
2024-02-19 20:50:04 +0700

    v-for, filter, image

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ const app = new Vue({
     axios.get('https://api.jikan.moe/v4/anime')
       .then(response => {
         this.anime = response.data.data;
+        console.log(this.anime)
+        
       })
       .catch(error => {
         this.error = error;
         console.error('Error fetching data:', error);
       });
   },
+  filters: {
+    truncate(text, length, suffix = "...") {
+      if (text.length <= length) {
+        return text;
+      }
+      return text.substring(0, length) + suffix;
+    }
+  }
 });
